8f895712c36bbfa0b745fd34c188377e0d919f4e
Bryan Giger <bryan@gigerbyt.es>
2020-11-11 17:27:38 +0000

    remove config file use env

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.8-slim-buster
 
 COPY ./app /app
-COPY config.py /config.py
+
 RUN pip install -r /app/requirements.txt
--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config.from_object('config.ProductionConfig')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
