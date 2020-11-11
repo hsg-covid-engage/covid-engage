6c0d91f73d06a0927aa80dca3754ac690f0a6df0
Bryan Giger <bryan@gigerbyt.es>
2020-11-11 17:22:05 +0000

    try copy config

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'abcdefgh'
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+class TestingConfig(Config):
+    TESTING = True
