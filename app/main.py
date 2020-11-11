8a374b5536864077489aefbf1091fe12b92b77f7
Bryan Giger <bryan@gigerbyt.es>
2020-11-11 15:00:01 +0000

    change port to 8080

--- app/main.py
+++ app/main.py
@@ -88,6 +88,6 @@ def logout():
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=8080)
 
     
